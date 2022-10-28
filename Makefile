656e3eea9756082ef700f12707fa78352bb0fbbd
Hyrum Nantais <hnantais@uoguelph.ca>
2022-10-28 13:10:44 -0400

    Create deployment commands

--- Makefile
+++ Makefile
@@ -12,3 +12,13 @@ production:
 	sudo service nginx restart
 	sudo killall gunicorn || true
 	gunicorn -w2 -b 0.0.0.0:5000 --chdir server 'wsgi:app' --daemon
+
+build_images:
+	echo glpat-2Fy4r4qfydMx59tAqVEj | docker login -u hnantais registry.socs.uoguelph.ca --password-stdin
+	docker build -t registry.socs.uoguelph.ca/hnantais/f22_cis3760_team106online/web web/
+	docker build -t registry.socs.uoguelph.ca/hnantais/f22_cis3760_team106online/server server/
+	docker push registry.socs.uoguelph.ca/hnantais/f22_cis3760_team106online/web
+	docker push registry.socs.uoguelph.ca/hnantais/f22_cis3760_team106online/server
+
+deploy_images:
+	curl -X POST https://\$AdvisorLink:kqk5nPXutJsibLHJTej75wcjyJovbxkwyLvLY7aznccvz8sKBHanPXZYNpwd@advisorlink.scm.azurewebsites.net/api/registry/webhook -d -H
--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -2,15 +2,15 @@ import Logo from '../static/logo_img.png';
 
 const Navbar = () => {
     return (
-        <div className="w-screen flex justify-center items-center p-6 z-10" id='Navbar'>
+        <div className="w-screen flex justify-center items-center p-2 z-10" id='Navbar'>
             <div className="w-screen flex bg-white justify-between">
-                <div className="flex justify-center items-center gap-4">
-                    <div>
+                <a href='/'>
+                    <div className="flex justify-center items-center gap-2 pl-3">
                         <img src={Logo} alt="advisorlink logo"/>
+                        <div className="text-4xl"><strong>Advisor</strong>Link</div>
                     </div>
-                    <div className="text-5xl"><strong>Advisor</strong>Link</div>
-                </div>
-                <div className="flex justify-center items-center gap-6 text-3xl">
+                </a>
+                <div className="flex justify-center items-center gap-8 text-2xl pr-10">
                     <div><a href='/'>Calendar</a></div>
                     <div><a href='/'>About Us</a></div>
                     <div><a href='/'>Contact</a></div>
