55f63dd09762fe45269bce338167a795cdd868e7
Rehan <rnagoorm@uoguelph.ca>
2022-11-01 20:35:02 -0400

    cookies functionality and adding courses and calendar

--- web/src/pages/Calendar.tsx
+++ web/src/pages/Calendar.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import FullCalendar from '@fullcalendar/react'
 import timeGridPlugin from '@fullcalendar/timegrid'
+import { useCookies } from 'react-cookie';
 
 const Calendar = () => {
+    const [cookies, setCookie] = useCookies(['courses']);
     return (
         <div className='section'>
             <div className='container w-full'>
@@ -15,10 +17,7 @@ const Calendar = () => {
                     }}
                     initialView='timeGridWeek'
                     weekends={true}
-                    events={[
-                        { title: 'CIS3210', startRecurrence: '2022-09-07', endRecurrence: '2022-12-07', daysOfWeek: [2, 4], startTime: '08:30', endTime: '10:30' },
-                        { title: 'CIS1500', startRecurrence: '2022-09-07', endRecurrence: '2022-12-07', daysOfWeek: [1, 3, 5], startTime: '12:30', endTime: '14:30', color: 'purple' },
-                    ]}
+                    events={cookies.courses}
                     slotMinTime='08:00:00'
                     slotMaxTime='24:00:00'
                     allDaySlot={false}
--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,13 +1,92 @@
 import React, { useState } from "react";
 import CourseList from "../components/CourseList";
+import { Cookies, useCookies } from "react-cookie";
+import { Console } from "console";
 
 const Home = () => {
+    const [cookies, setCookie] = useCookies(['courses']);
+    //array of meeting information (containing temp information)
+    let meetingList = [{ title: 'CIS3210', startRecurrence: '2022-09-07', endRecurrence: '2022-12-07', daysOfWeek: [2, 4], startTime: '08:30', endTime: '10:30' },
+    { title: 'CIS2500', startRecurrence: '2022-09-07', endRecurrence: '2022-12-07', daysOfWeek: [1, 3, 5], startTime: '12:30', endTime: '14:30', color: 'purple' },] 
+
+    // function should add course to the meeting list
+    // function addCourseToMeeting({ course1 }) {
+        
+    //     let course = {"name": "Intro Financial Accounting",
+    //         "faculty": "ACCT",
+    //         "code": "1220",
+    //         "number": "0108",
+    //         "weight": "0.50",
+    //         "level": "Undergraduate",
+    //         "instructor": "P. Lassou",
+    //         "term": "Fall 2022",
+    //         "location": "Guelph",
+    //         "capacity": "70",
+    //         "enrolled": "2",
+    //         "meetings": [
+    //             {
+    //                 "type": "LEC",
+    //                 "days": [
+    //                     "Fri"
+    //                 ],
+    //                 "start_time": "08:30AM",
+    //                 "end_time": "10:20AM",
+    //                 "date": null,
+    //                 "building": "ROZH",
+    //                 "room": "104"
+    //             },
+    //             {
+    //                 "type": "SEM",
+    //                 "days": [
+    //                     "Wed"
+    //                 ],
+    //                 "start_time": "01:30PM",
+    //                 "end_time": "02:20PM",
+    //                 "date": null,
+    //                 "building": "AD-S",
+    //                 "room": "VIRTUAL"
+    //             },
+    //             {
+    //                 "type": "EXAM",
+    //                 "days": [
+    //                     "Tues"
+    //                 ],
+    //                 "start_time": "08:30AM",
+    //                 "end_time": "10:30AM",
+    //                 "date": "2022/12/06",
+    //                 "building": null,
+    //                 "room": null
+    //             }
+    //         ]
+    //     }
+
+    //     let startDate = '';
+    //     let endDate = '';
+    //     let startTime = course.meetings[0].start_time;
+    //     let endTime = course.meetings[0].end_time;
+
+    //     // assign start and endDate based off of term
+    //     if (course.term == "Fall 2022") {
+    //         startDate = '2022-09-10';
+    //         endDate = '2022-12-20';
+    //     }
+    //     //assign start and endtime for each meeting
+    //     //assign days of the week for each meeting
+
+    //     //add item in the json format required
+    //     meetingList.push({title: course.faculty+course.code, startRecurrence: startDate, endRecurrence: endDate, daysOfWeek: [2, 4], startTime: startTime, endTime: endTime});
+    // }
+
     const [clickedButton, setClickedButton] = useState(false);
     const buttonHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
         event.preventDefault();
 
         const button: HTMLButtonElement = event.currentTarget;
         setClickedButton(true);
+
+        // this would be in the add course button handler
+        setCookie('courses', meetingList, { path: '/' });
+        console.log(cookies.courses);
     }
 
     return (
