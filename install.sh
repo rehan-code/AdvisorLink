401e82920686600b7deddfa0c7f66e0e70b4a5e0
swguilbeault <swguilbeault@icloud.com>
2022-11-05 12:23:57 -0400

    add installation steps for docker

--- install.sh
+++ install.sh
@@ -24,3 +24,18 @@ yarn build
 sudo apt install -y nginx
 sudo cp nginx/nginx.conf /etc/nginx/sites-available/default
 sudo cp -r build/* /usr/share/nginx/html
+
+# Installation steps for Docker => Postgres
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+sudo mkdir /etc/ssl/certs || true
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -sudo apt-key fingerprint 0EBFCD88sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo docker pull postgres
