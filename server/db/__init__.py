7cc4080bb981ff4bf3f7581e5838a91982ae1b04
swguilbeault <swguilbeault@icloud.com>
2022-11-05 12:24:28 -0400

    add db models

--- server/db/__init__.py
+++ server/db/__init__.py
@@ -1,1 +1,2 @@
 from .base import db
+from . import models
---
+++ server/db/models/term.py
@@ -0,0 +1,18 @@
+from ..base import db
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
+from ..utils.uuid_from_string import uuid_from_str
+
+class Term(db.Model):
+    __tablename__ = 'term'
+
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    name = db.Column(db.VARCHAR(length=255))
+    start_date = db.Column(db.Date())
+    end_date = db.Column(db.Date())
+
+    def __init__(self, name, start_date, end_date):
+        self.id = uuid_from_str(name)
+        self.name = name
+        self.start_date = start_date
+        self.end_date = end_date
