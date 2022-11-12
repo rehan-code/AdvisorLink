860e5a49bc1833d07da39fc9ddda05ccbde46f01
Alif Merchant <amerch04@uoguelph.ca>
2022-11-12 13:36:25 -0500

    Added schedule export functionality

--- web/src/components/Schedule.tsx
+++ web/src/components/Schedule.tsx
@@ -5,6 +5,7 @@
 import React from 'react';
 import FullCalendar, { EventSourceInput } from '@fullcalendar/react';
 import timeGridPlugin from '@fullcalendar/timegrid';
+import jsPDF from 'jspdf';
 import { Section } from './CalendarComponent';
 
 const daysOfWeek = ['MON', 'TUES', 'WED', 'THUR', 'FRI', 'SAT', 'SUN'];
@@ -21,7 +22,7 @@ interface Event {
 function getDaysOfWeekInteger(days: string[]) {
   const daysInt: number[] = [];
   days.forEach((day) => {
-    // FIX THIS - why am I getting days in 'DayOfWeek.DAY' format
+    // FIX THIS - days in 'DayOfWeek.DAY' format
     daysInt.push(daysOfWeek.indexOf(day.substring(10)) + 1);
   });
 
@@ -37,8 +38,7 @@ function createCalendarEvents(sections: Section[]) {
       .forEach((meeting) => {
         events.push({
           // FIX THIS - meeting type in 'MeetingType.TYPE' format
-          title: `${section.faculty}*${section.code}*${
-            section.number
+          title: `${section.faculty}*${section.code}*${section.number
           }\n${meeting.type.substring(12)}`,
           daysOfWeek: getDaysOfWeekInteger(meeting.days),
           startTime: meeting.start_time,
@@ -51,30 +51,50 @@ function createCalendarEvents(sections: Section[]) {
   return events;
 }
 
+const handleExport = async () => {
+  const htmlElement: HTMLElement = document.getElementById('sc') as HTMLElement;
+  // eslint-disable-next-line new-cap
+  const report = new jsPDF('landscape', 'mm', [htmlElement.scrollWidth + 1, htmlElement.scrollHeight + 1]);
+  report.html(htmlElement).then(() => {
+    report.save('schedule.pdf');
+  });
+};
+
 export default function Schedule(props: any) {
   return (
     <div className="bg-white">
-      <h1 className="text-2xl font-bold pb-8">Course Schedule</h1>
-      <FullCalendar
-        plugins={[timeGridPlugin]}
-        headerToolbar={{
-          left: '',
-          center: '',
-          right: '',
-        }}
-        dayHeaderFormat={{
-          weekday: 'short',
-        }}
-        initialView="timeGridWeek"
-        weekends
-        eventDidMount={function (_info) {}}
-        events={createCalendarEvents(props.events)}
-        slotMinTime="07:00:00"
-        slotMaxTime="23:00:00"
-        allDaySlot={false}
-        contentHeight="auto"
-        slotEventOverlap={false}
-      />
+      <div id="sc">
+        <h1 className="text-2xl font-bold pb-8">Course Schedule</h1>
+        <FullCalendar
+          plugins={[timeGridPlugin]}
+          headerToolbar={{
+            left: '',
+            center: '',
+            right: '',
+          }}
+          dayHeaderFormat={{
+            weekday: 'short',
+          }}
+          initialView="timeGridWeek"
+          weekends
+          eventDidMount={function (info) { }}
+          events={createCalendarEvents(props.events)}
+          slotMinTime="07:00:00"
+          slotMaxTime="23:00:00"
+          allDaySlot={false}
+          contentHeight="auto"
+          slotEventOverlap={false}
+        />
+      </div>
+      <div className="flex justify-evenly items-center p-4">
+        <button
+          className="py-4 px-8 bg-blue-500 rounded-md text-white hover:bg-blue-800 font-bold"
+          type="submit"
+          onClick={handleExport}
+        >
+          Export
+        </button>
+      </div>
     </div>
   );
 }
